7e5a80334d1b07d5f2ab37d8818b3bc9e540dde4
박성원 <skfhddlg@naver.com>
2023-08-15 17:57:33 +0900

    키보드액티비티 참고해 usb키입력을 블루투스키입력으로 전달기능 추가

--- app/src/main/java/com/blackshark/hidperipheral/MainActivity.kt
+++ app/src/main/java/com/blackshark/hidperipheral/MainActivity.kt
@@ -8,6 +8,8 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.view.KeyEvent.*
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -90,6 +92,7 @@ class MainActivity : AppCompatActivity(), HidUtils.ConnectionStateChangeListener
 
         GlobalScope.launch(Dispatchers.Main) {
             binding.tvConnectStatus.text = "${getString(R.string.connected)} : ${HidUtils.mDevice!!.name}"
+            HidConsts.cleanKbd()
         }
 
     }
@@ -99,5 +102,121 @@ class MainActivity : AppCompatActivity(), HidUtils.ConnectionStateChangeListener
             binding.tvConnectStatus.text = getString(R.string.ununited)
         }
 
+    }
+
+    override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
+        binding.tvConnectStatus.text = "$event"
+        when(event?.action)
+        {
+            ACTION_DOWN -> HidConsts.kbdKeyDown(keyCode2usageStr(event.keyCode))
+            ACTION_UP -> HidConsts.kbdKeyUp(keyCode2usageStr(event.keyCode))
+            else -> return true
+        }
+        return false
+    }
+
+    private fun keyCode2usageStr(keyCode: Int) = when (keyCode) {
+        KEYCODE_ESCAPE -> "41"
+        KEYCODE_F1 -> "58"
+        KEYCODE_F2 -> "59"
+        KEYCODE_F3 -> "60"
+        KEYCODE_F4 -> "61"
+        KEYCODE_F5 -> "62"
+        KEYCODE_F6 -> "63"
+        KEYCODE_F7 -> "64"
+        KEYCODE_F8 -> "65"
+        KEYCODE_F9 -> "66"
+        KEYCODE_F10 -> "67"
+        KEYCODE_F11 -> "68"
+        KEYCODE_F12 -> "69"
+        KEYCODE_FORWARD_DEL -> "76"
+        KEYCODE_MOVE_END -> "77"
+        KEYCODE_GRAVE -> "53" //"`  ~"
+        KEYCODE_1 -> "30"
+        KEYCODE_2 -> "31"
+        KEYCODE_3 -> "32"
+        KEYCODE_4 -> "33"
+        KEYCODE_5 -> "34"
+        KEYCODE_6 -> "35"
+        KEYCODE_7 -> "36"
+        KEYCODE_8 -> "37"
+        KEYCODE_9 -> "38"
+        KEYCODE_0 -> "39"
+        KEYCODE_MINUS -> "45"
+        KEYCODE_EQUALS -> "46" //"=  +"
+        KEYCODE_DEL -> "42" //"Backspace"
+        KEYCODE_TAB -> "43"
+        KEYCODE_Q -> "20"
+        KEYCODE_W -> "26"
+        KEYCODE_E -> "8"
+        KEYCODE_R -> "21"
+        KEYCODE_T -> "23"
+        KEYCODE_Y -> "28"
+        KEYCODE_U -> "24"
+        KEYCODE_I -> "12"
+        KEYCODE_O -> "18"
+        KEYCODE_P -> "19"
+        KEYCODE_LEFT_BRACKET -> "47" //"[  {"
+        KEYCODE_RIGHT_BRACKET -> "48" //"]  }"
+        KEYCODE_BACKSLASH -> "49" // \ or |
+        KEYCODE_CAPS_LOCK -> "57"
+        KEYCODE_A -> "4"
+        KEYCODE_S -> "22"
+        KEYCODE_D -> "7"
+        KEYCODE_F -> "9"
+        KEYCODE_G -> "10"
+        KEYCODE_H -> "11"
+        KEYCODE_J -> "13"
+        KEYCODE_K -> "14"
+        KEYCODE_L -> "15"
+        KEYCODE_SEMICOLON -> "51" //";  :"
+        KEYCODE_APOSTROPHE -> "52" // ' or "
+        KEYCODE_ENTER -> "40"
+        KEYCODE_SHIFT_LEFT -> "M2"
+        KEYCODE_Z -> "29"
+        KEYCODE_X -> "27"
+        KEYCODE_C -> "6"
+        KEYCODE_V -> "25"
+        KEYCODE_B -> "5"
+        KEYCODE_N -> "17"
+        KEYCODE_M -> "16"
+        KEYCODE_COMMA -> "54"
+        KEYCODE_PERIOD -> "55"
+        KEYCODE_SLASH -> "56"
+        KEYCODE_SHIFT_RIGHT -> "M32"
+        KEYCODE_CTRL_LEFT -> "M1"
+        KEYCODE_WINDOW -> "M8"
+        KEYCODE_ALT_LEFT -> "M4"
+        KEYCODE_SPACE -> "44"
+        KEYCODE_ALT_RIGHT -> "M4"
+        KEYCODE_MENU -> "101"
+        KEYCODE_SYSRQ -> "70" //Prt
+        KEYCODE_CTRL_RIGHT -> "M16"
+        KEYCODE_INSERT -> "75"
+        KEYCODE_DPAD_UP -> "82"
+        KEYCODE_PAGE_UP -> "73"
+        KEYCODE_PAGE_DOWN -> "78"
+        KEYCODE_MOVE_HOME -> "74"
+        KEYCODE_SCROLL_LOCK -> "71"
+        KEYCODE_BREAK -> "71"
+        KEYCODE_NUM_LOCK -> "83"
+        KEYCODE_NUMPAD_DIVIDE -> "84"
+        KEYCODE_NUMPAD_MULTIPLY -> "85"
+        KEYCODE_NUMPAD_SUBTRACT -> "86"
+        KEYCODE_NUMPAD_ADD -> "87"
+        KEYCODE_DPAD_LEFT -> "80"
+        KEYCODE_DPAD_DOWN -> "81"
+        KEYCODE_DPAD_RIGHT -> "79"
+        KEYCODE_NUMPAD_1 -> "89"
+        KEYCODE_NUMPAD_2 -> "90"
+        KEYCODE_NUMPAD_3 -> "91"
+        KEYCODE_NUMPAD_4 -> "93"
+        KEYCODE_NUMPAD_5 -> "93"
+        KEYCODE_NUMPAD_6 -> "94"
+        KEYCODE_NUMPAD_7 -> "95"
+        KEYCODE_NUMPAD_8 -> "96"
+        KEYCODE_NUMPAD_9 -> "97"
+        KEYCODE_NUMPAD_ENTER -> "88"
+        else -> ""
     }
 }
